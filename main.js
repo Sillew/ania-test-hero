a1d5ab2fd1838d473e653bafca384a6e2d7f0652
Sillew <105428054+Sillew@users.noreply.github.com>
2025-11-08 14:03:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const blogs = document.querySelectorAll(".blog > div");
+const modal = document.querySelector(".blog-modal");
+
+blogs.forEach((blog) => {
+  blog.addEventListener("click", () => {
+    modal.innerHTML = `
+      <div class="blog-preview">
+        <button class="close-btn"><i class="fa-solid fa-xmark"></i></button>
+        ${blog.innerHTML}
+      </div>`;
+    modal.classList.add("active");
+
+    // blokada scrollowania tła
+    document.body.style.overflow = "hidden";
+  });
+});
+
+// zamykanie po kliknięciu w tło lub w X
+modal.addEventListener("click", (e) => {
+  if (e.target === modal || e.target.closest(".close-btn")) {
+    modal.classList.remove("active");
+    modal.innerHTML = "";
+    document.body.style.overflow = ""; // przywrócenie scrolla
+  }
+});
